a650a63027bbbc57014700d9aeff384524cf372a
Hamish <hamish@hamishwest.xyz>
2019-04-13 13:54:05 +0930

    Adds first pass of graphics and some extra control variables

--- GameOfLife.m
+++ GameOfLife.m
@@ -1,23 +1,37 @@
 % This script is a simple version of Conway's Game of Life
 
-% For now assume a 20x100 play area (Usable 28x98)
-% A is the current state to be assesed, B is the next turn.
-A = zeros(20, 100);
-B = zeros(20, 100);
+% TODO items:
+% Seeds and seed strings!
+% Factions
+% Finer user control and settings
+% GFX!
+
+%%%%% Define User Configurable Variables
+% For now assume a 100x100 play area (Usable 98x98)
+mapSizeCols = 100;
+mapSizeRows = 100;
+simulationSpeed = 20;
+consoleOutput = false;
+
+%%%%% Define Game Mode variables
+currentBoard = zeros(mapSizeRows, mapSizeCols);
+newBoard = zeros(mapSizeRows, mapSizeCols);
 CurrentTurn = 0;
+mainFig = figure('Name','Game of Life', 'WindowState', 'maximized');
+
 
 % Add a map seed
 for i = [1:5]
     for j = [1:5]
-        A(i,j) = 1;
+        currentBoard(i,j) = 1;
     end
 end
 
 % Lock the game loop on, eventually this will be a toggle
 while 1
     % Iterate through all cells in the grid
-    for i = [2: 19]
-        for j = [2:99]
+    for i = [2: mapSizeRows - 1]
+        for j = [2: mapSizeCols - 1]
             % Now we are at the cell to check
             AliveCells = 0;
             
@@ -31,7 +45,7 @@ while 1
                     end
                     
                     % Is this cell dead or alive?
-                    if A(i + k, j + l) == 1
+                    if currentBoard(i + k, j + l) == 1
                         AliveCells = AliveCells + 1;
                     end
                 end
@@ -39,31 +53,37 @@ while 1
             % Check cell status and then set B(i,j) to dead or alive.
             
             if AliveCells < 2
-                B(i, j) = 0;
+                newBoard(i, j) = 0;
             elseif AliveCells == 2 || AliveCells == 3
-                B(i, j) = 1;
+                newBoard(i, j) = 1;
             elseif AliveCells > 3
-                B(i, j) = 0;
-            elseif A(i, j) == 0 && AliveCells == 3
-                B(i, j) = 1;
+                newBoard(i, j) = 0;
+            elseif currentBoard(i, j) == 0 && AliveCells == 3
+                newBoard(i, j) = 1;
             end
         end 
     end
     
-    % Print new board to the command window
-    clc;
-    for i = [2: 19]
-        rowToPrint = "";
-        for j = [2:99]
-            rowToPrint = strcat(rowToPrint, num2str(B(i,j)));
+    if consoleOutput == true
+        % Print new board to the command window
+        clc;
+        for i = [2: 19]
+            rowToPrint = "";
+            for j = [2:99]
+                rowToPrint = strcat(rowToPrint, num2str(newBoard(i,j)));
+            end
+            fprintf(rowToPrint + "\n");
         end
-        fprintf(rowToPrint + "\n");
+
+        fprintf("Current turn: %d\n", CurrentTurn);
     end
     
-    fprintf("Current turn: %d\n", CurrentTurn);
     % Copy new board to old board, reset new board and go to next turn
-    A = B;
-    B = zeros(20,100);
+    currentBoard = newBoard;
+    newBoard = zeros(mapSizeRows, mapSizeCols);
     CurrentTurn = CurrentTurn + 1;
-    pause(.5);
+    spy(currentBoard);
+    
+    % Check for user input and break/continue. TODO
+    pause(.5 / simulationSpeed);
 end
