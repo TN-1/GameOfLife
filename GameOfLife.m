1514fb5176f9acbb0f41381cefc9663f5b4fb4bc
Hamish <hamish@hamishwest.xyz>
2019-04-30 18:03:47 +0930

    Clean up after merge

--- GameOfLife.m
+++ GameOfLife.m
@@ -17,24 +17,21 @@ mapSizeCols = 100;
 mapSizeRows = 100;
 simulationSpeed = 5; % 10 is getting laggy
 consoleOutput = false;
-seedString = '32321550101299';
+seedString = '32321224632122'; %3232188 is fun
+constSeed = true;
 
 %%% Define Game Mode variables
-currentBoard = 2 * zeros(mapSizeRows, mapSizeCols);
-newBoard = 2 * zeros(mapSizeRows, mapSizeCols);
+currentBoard = zeros(mapSizeRows, mapSizeCols);
+newBoard = zeros(mapSizeRows, mapSizeCols);
+seedBoard = zeros(mapSizeRows, mapSizeCols);
 CurrentTurn = 0;
 mainFig = figure('Name','Game of Life',...
     'WindowState', 'maximized',...
     'MenuBar', 'none',...
     'ToolBar', 'none',...
     'DockControls', 'off'...
-);
+    );
 set(mainFig, 'KeyPressFcn', @figureKeyPressHandler);
-colorMap = [1      1      1;
-            0      1      0;
-            0      0      1];
-        
-        
 
 %% Start Game Mode
 % Seed the map
@@ -58,45 +55,107 @@ for i = 1:7:length(seedString)
     end
 end
 
+seedBoard = currentBoard;
 %return;
 
 % Start game loop
-while ~isPaused   
+while ~isPaused
     % Iterate through all cells in the grid
     for i = 2: mapSizeRows - 1
         for j = 2: mapSizeCols - 1
             % Now we are at the cell to check
             AliveCells = 0;
-
-            % Next nested loop is the border squares to check
-            for k = -1:1
-                for l = -1:1
-                    if k == 0 && l == 0
-                        % If we are on the current cell (i,j), ignore it from
-                        % the search
-                        continue;
+            
+            switch(currentBoard(i, j))
+                case 0
+                    alive1 = 0;
+                    alive2 = 0;
+                    for k = -1:1
+                        for l = -1:1
+                            if k == 0 && l == 0
+                                % If we are on the current cell (i,j), ignore it from
+                                % the search
+                                continue;
+                            end
+                            
+                            % Is this cell dead or alive?
+                            if currentBoard(i + k, j + l) ~= 0
+                                AliveCells = AliveCells + 1;
+                                if currentBoard(i + k, j + l) == 1
+                                    alive1 = alive1 + 1;
+                                elseif currentBoard(i + k, j + l) == 2
+                                    alive2 = alive2 + 1;
+                                end
+                            end
+                        end
                     end
-
-                    % Is this cell dead or alive?
-                    if currentBoard(i + k, j + l) == 1
-                        AliveCells = AliveCells + 1;
+                    
+                    % Check cell status and then set B(i, j) to dead or alive.
+                    if AliveCells == 3
+                        if alive1 > alive2
+                            newBoard(i, j) = 1;
+                        else
+                            newBoard(i, j) = 2;
+                        end
+                    else
+                        newBoard(i, j) = 0;
                     end
-                end
+                    
+                case 1
+                    % Next nested loop is the border squares to check
+                    for k = -1:1
+                        for l = -1:1
+                            if k == 0 && l == 0
+                                % If we are on the current cell (i,j), ignore it from
+                                % the search
+                                continue;
+                            end
+                            
+                            % Is this cell dead or alive?
+                            if currentBoard(i + k, j + l) == 1
+                                AliveCells = AliveCells + 1;
+                            end
+                        end
+                    end
+                    % Check cell status and then set B(i, j) to dead or alive.
+                    
+                    if AliveCells < 2
+                        newBoard(i, j) = 0;
+                    elseif AliveCells == 2 || AliveCells == 3
+                        newBoard(i, j) = 1;
+                    elseif AliveCells > 3
+                        newBoard(i, j) = 0;
+                    end
+                    
+                case 2
+                    % Next nested loop is the border squares to check
+                    for k = -1:1
+                        for l = -1:1
+                            if k == 0 && l == 0
+                                % If we are on the current cell (i,j), ignore it from
+                                % the search
+                                continue;
+                            end
+                            
+                            % Is this cell dead or alive?
+                            if currentBoard(i + k, j + l) == 2
+                                AliveCells = AliveCells + 1;
+                            end
+                        end
+                    end
+                    % Check cell status and then set B(i, j) to dead or alive.
+                    
+                    if AliveCells < 2
+                        newBoard(i, j) = 0;
+                    elseif AliveCells == 2 || AliveCells == 3
+                        newBoard(i, j) = 2;
+                    elseif AliveCells > 3
+                        newBoard(i, j) = 0;
+                    end
             end
-            % Check cell status and then set B(i, j) to dead or alive.
-
-            if AliveCells < 2
-                newBoard(i, j) = 0;
-            elseif AliveCells == 2 || AliveCells == 3
-                newBoard(i, j) = 1;
-            elseif AliveCells > 3
-                newBoard(i, j) = 0;
-            elseif currentBoard(i, j) == 0 && AliveCells == 3
-                newBoard(i, j) = 1;
-            end
-        end 
+        end
     end
-
+    
     % Check if we are configured for console output
     %%% TODO: Needs to be reworked for variable board size
     if consoleOutput == true
@@ -109,29 +168,27 @@ while ~isPaused
             end
             fprintf(rowToPrint + "\n");
         end
-
+        
         fprintf("Current turn: %d\n", CurrentTurn);
     end
-
+    
     % Copy new board to old board, reset new board, output the new board to the figure and go to next turn
-    currentBoard = newBoard;
-    newBoard = 2 * zeros(mapSizeRows, mapSizeCols);
+    currentBoard = newBoard | seedBoard;
+    newBoard = zeros(mapSizeRows, mapSizeCols);
     CurrentTurn = CurrentTurn + 1;
     clf;
     spy(currentBoard);
-    %colorbar();
-    %colormap(colorMap);
     pause(.5 / simulationSpeed);
 end
-    
+
 %% Functions
 % Handles keypress for the figure window. YAY for events :)
 function figureKeyPressHandler(~, event)
-    global isPaused;
-    
-    switch(event.Key)
-        case 'c'
-            isPaused = ~isPaused;
-            close all;
-    end
-end     
+global isPaused;
+
+switch(event.Key)
+    case 'c'
+        isPaused = ~isPaused;
+        close all;
+end
+end
