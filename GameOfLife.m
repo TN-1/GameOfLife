caae2134b9e4d94693d97dbf209f90055cb0ad0b
Hamish <hamish@hamishwest.xyz>
2019-04-30 16:57:16 +0930

    End FireSimBranch. Next commit will merge these upstream changes to master

--- GameOfLife.m
+++ GameOfLife.m
@@ -16,11 +16,11 @@ mapSizeRows = 256;
 simulationSpeed = 10; % 10 is getting laggy
 consoleOutput = false;
 fireStrength = 8; % This will eventually be replaced by maths and data
-seedString = '30306FF010125530016FF';
+seedString = '010135530306FF30016FFE0E0155';
 
 %%%%% Define Game Mode variables
-currentBoard = ones(mapSizeRows, mapSizeCols);
-newBoard = ones(mapSizeRows, mapSizeCols);
+currentBoard = 2 * ones(mapSizeRows, mapSizeCols);
+newBoard = 2 * ones(mapSizeRows, mapSizeCols);
 CurrentTurn = 0;
 mainFig = figure('Name','Game of Life',...
     'WindowState', 'maximized',...
@@ -68,7 +68,7 @@ while ~isPaused
             BurningCells = 0;
             
             % Lets so if this cell can be skipped
-            if currentBoard(i, j) == 0 || currentBoard(i, j) == 6
+            if currentBoard(i, j) == 1 || currentBoard(i, j) == 6
                 newBoard(i, j) = currentBoard(i, j);
                 continue;
             end
@@ -83,7 +83,7 @@ while ~isPaused
                     end
 
                     % Is this cell dead or alive?
-                    if currentBoard(i + k, j + l) >= 2 && currentBoard(i + k, j + l) <= 5
+                    if currentBoard(i + k, j + l) >= 3 && currentBoard(i + k, j + l) <= 5
                         BurningCells = BurningCells + 1;
                     end
                 end
@@ -91,15 +91,15 @@ while ~isPaused
             
             % Check cell status and then set B(i, j) to dead or alive.
             if BurningCells < 2
-                newBoard(i, j) = 1;
+                newBoard(i, j) = 2;
             elseif BurningCells >= 2 || BurningCells <= fireStrength
-                newBoard(i, j) = 2;
-            elseif currentBoard(i, j) == 1 && BurningCells == 3
-                newBoard(i, j) = 2;
+                newBoard(i, j) = 3;
+            elseif currentBoard(i, j) == 2 && BurningCells == 3
+                newBoard(i, j) = 3;
             end
             
             % Check if cell is burning, if so, age it
-            if currentBoard(i, j) >= 2 && currentBoard(i, j) <= 5
+            if currentBoard(i, j) >= 3 && currentBoard(i, j) <= 5
                 newBoard(i, j) = currentBoard(i, j) + 1;
             end
         end 
@@ -123,11 +123,11 @@ while ~isPaused
 
     % Copy new board to old board, reset new board, output the new board to the figure and go to next turn
     currentBoard = newBoard;
-    newBoard = ones(mapSizeRows, mapSizeCols);
+    newBoard = 2 * ones(mapSizeRows, mapSizeCols);
     CurrentTurn = CurrentTurn + 1;
     clf;
-    cspy(currentBoard);
-    colorbar('off');
+    imagesc(currentBoard);
+    colorbar();
     colormap(colorMap);
     pause(.5 / simulationSpeed);
 end
